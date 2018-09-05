4d6f5be1bb05b985e88b8c19e1b7db0c6cbb7f94
Peter OKoth <pokoth2009@gmail.com>
2018-09-06 01:18:26 +0300

    added app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+from flask import jsonify
+import json
+import requests
+
+import pdb; pdb.set_trace()
+
+app = Flask(__name__)
+
+
+
+orders = { }
+
+@app.route('/api/v1/orders')
+def get_all_orders():
+    return jsonify({'orders':orders})
+
+
+if __name__ == '__main__':
+    app.run()
